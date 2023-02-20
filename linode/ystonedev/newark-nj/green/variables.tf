8f1ccf9df94cf316fa4693e5961e2ee0a4faf6f3
= <=>
2023-02-20 17:18:12 -0500

    update formatting

--- linode/ystonedev/newark-nj/green/variables.tf
+++ linode/ystonedev/newark-nj/green/variables.tf
@@ -1,41 +1,38 @@
+variable "token" {
+    description = "Linode API Key (required)"
+}
 
-    variable "token" {
-        description = "Linode API Key (required)"
+variable "k8s_version" {
+  description = "The Kubernetes version to use for this cluster. (required)"
+  default = "1.25"
+}
 
+variable "label" {
+  description = "The unique label to assign to this cluster. (required)"
+  default = "default-lke-cluster"
+}
+
+variable "region" {
+  description = "The region where your cluster will be located. (required)"
+  default = "us-east"
+}
+
+variable "tags" {
+  description = "Tags to apply to your cluster for organizational purposes. (optional)"
+  type = list(string)
+  default = ["test"]
+}
+
+variable "pools" {
+  description = "The Node Pool specifications for the Kubernetes cluster. (required)"
+  type = list(object({
+    type = string
+    count = number
+  }))
+  default = [
+    {
+      type = "g6-standard-2"
+      count = 2
     }
-
-    variable "k8s_version" {
-      description = "The Kubernetes version to use for this cluster. (required)"
-      default = "1.23"
-    }
-
-    variable "label" {
-      description = "The unique label to assign to this cluster. (required)"
-      default = "default-lke-cluster"
-    }
-
-    variable "region" {
-      description = "The region where your cluster will be located. (required)"
-      default = "us-east"
-    }
-
-    variable "tags" {
-      description = "Tags to apply to your cluster for organizational purposes. (optional)"
-      type = list(string)
-      default = ["test"]
-    }
-
-    variable "pools" {
-      description = "The Node Pool specifications for the Kubernetes cluster. (required)"
-      type = list(object({
-        type = string
-        count = number
-      }))
-      default = [
-        {
-          type = "g6-standard-2"
-          count = 2
-        }
-      ]
-    }
-    
+  ]
+}
